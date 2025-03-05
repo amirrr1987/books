1112986a1f693d313456a10d3a6549984e792439
Amir Maghami <Maghami.A1987@gmail.com>
2025-03-05 10:47:20 +0330

    Create Image1.vue

---
+++ components/Image1.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex justify-center my-12 flex-col items-center gap-4">
+    <img :src="`/images/refactoring-ui/${props.name}`" alt="ui" />
+  </div>
+</template>
+<script setup lang="ts">
+interface Props {
+  name: string;
+}
+const props = withDefaults(defineProps<Props>(), {});
+</script>
