64dd8aa7bb569b33c08952de2298b189f97c19e7
Amir Maghami <Maghami.A1987@gmail.com>
2025-03-05 10:47:23 +0330

    Create Image2.vue

---
+++ components/Image2.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex justify-center my-12 flex-col items-center gap-4">
+    <img v-if="props.isBad" class="w-8 h-8" src="/images/refactoring-ui/0.png" alt="ui" />
+    <img v-else class="w-8 h-8" src="/images/refactoring-ui/1.png" alt="ui" />
+    <img :src="`/images/refactoring-ui/${props.name}`" alt="ui" />
+  </div>
+</template>
+<script setup lang="ts">
+interface Props {
+  name: string;
+  isBad: false
+}
+const props = withDefaults(defineProps<Props>(), {});
+</script>
