2c98ab5d79161b86f638307bdb0541756e2a1224
Amir Maghami <Maghami.A1987@gmail.com>
2025-03-08 12:25:24 +0330

    Update Image2.vue

--- components/Image2.vue
+++ components/Image2.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="flex justify-center my-12 flex-col items-center gap-4">
-    <img v-if="props.isBad" class="w-8 h-8" src="/images/refactoring-ui/0.png" alt="ui" />
-    <img v-else class="w-8 h-8" src="/images/refactoring-ui/1.png" alt="ui" />
+    <img  class="w-8 h-8" src="/images/refactoring-ui/good.png" alt="ui" />
+    <img  class="w-8 h-8" src="/images/refactoring-ui/bad.png" alt="ui" />
+    <img class="w-8 h-8" src="/images/refactoring-ui/question.png" alt="ui" />
     <img :src="`/images/refactoring-ui/${props.name}`" alt="ui" />
   </div>
 </template>
 <script setup lang="ts">
 interface Props {
   name: string;
-  isBad: false
+  mood: false
 }
 const props = withDefaults(defineProps<Props>(), {});
 </script>
