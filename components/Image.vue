26621b3a2efb3c24918f2b9bd0336901c44fc0e4
Amir Maghami <Maghami.A1987@gmail.com>
2025-03-08 12:24:53 +0330

    Create Image.vue

---
+++ components/Image.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="flex flex-col items-center gap-4 my-12">
+    <div
+      v-if="double"
+      class="flex"
+      :style="{ columnGap: `${props.double.gap}rem` }"
+    >
+      <img
+        class="w-8 h-8"
+        :src="`/images/refactoring-ui/${props.double.sort[0]}.png`"
+        alt="label"
+      />
+
+      <img
+        class="w-8 h-8"
+        :src="`/images/refactoring-ui/${props.double.sort[0]}.png`"
+        alt="label"
+      />
+    </div>
+
+    <div v-if="single" class="flex">
+      <img
+        class="w-8 h-8"
+        :src="`/images/refactoring-ui/${props.single}.png`"
+        alt="label"
+      />
+    </div>
+
+    <img :src="`/images/refactoring-ui/${props.name}`" alt="main" />
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  name: string;
+  single: "bad" | "good" | "question";
+  double: {
+    sort: ["bad", "good"] | ["good", "bad"];
+    gap: number;
+  };
+}
+
+const props = defineProps<Props>();
+</script>
