5360421c533f82eba9b0f32e0bd47eb3d24252a2
Amir Maghami <Maghami.A1987@gmail.com>
2025-03-09 09:08:36 +0330

    fix

--- components/Image.vue
+++ components/Image.vue
@@ -1,33 +1,35 @@
 <template>
-  <div class="flex flex-col items-center gap-4 my-12">
-    <div
-      v-if="double"
-      class="flex"
-      :style="{ columnGap: `${props.double.gap}rem` }"
-    >
-      <img
-        class="w-8 h-8"
-        :src="`/images/refactoring-ui/${props.double.sort[0]}.png`"
-        alt="label"
-      />
+  <a :href="`/images/refactoring-ui/${props.name}`">
+    <div class="flex flex-col items-center gap-4 my-12">
+      <div
+        v-if="double"
+        class="flex"
+        :style="{ columnGap: `${props.double.gap}rem` }"
+      >
+        <img
+          class="w-8 h-8"
+          :src="`/images/refactoring-ui/${props.double.sort[0]}.png`"
+          alt="label"
+        />
 
-      <img
-        class="w-8 h-8"
-        :src="`/images/refactoring-ui/${props.double.sort[0]}.png`"
-        alt="label"
-      />
+        <img
+          class="w-8 h-8"
+          :src="`/images/refactoring-ui/${props.double.sort[0]}.png`"
+          alt="label"
+        />
+      </div>
+
+      <div v-if="single" class="flex">
+        <img
+          class="w-8 h-8"
+          :src="`/images/refactoring-ui/${props.single}.png`"
+          alt="label"
+        />
+      </div>
+
+      <img :src="`/images/refactoring-ui/${props.name}`" alt="main" />
     </div>
-
-    <div v-if="single" class="flex">
-      <img
-        class="w-8 h-8"
-        :src="`/images/refactoring-ui/${props.single}.png`"
-        alt="label"
-      />
-    </div>
-
-    <img :src="`/images/refactoring-ui/${props.name}`" alt="main" />
-  </div>
+  </a>
 </template>
 
 <script setup lang="ts">
