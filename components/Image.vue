4995e67d04eac814fc9b53e7afd6e71666965b84
Amir Maghami <Maghami.A1987@gmail.com>
2025-03-17 10:25:38 +0330

    Update Image.vue

--- components/Image.vue
+++ components/Image.vue
@@ -1,45 +1,15 @@
 <template>
-  <a :href="`/images/refactoring-ui/${props.name}`">
-    <div class="flex flex-col items-center gap-4 my-12">
-      <div
-        v-if="double"
-        class="flex"
-        :style="{ columnGap: `${props.double.gap}rem` }"
-      >
-        <img
-          class="w-8 h-8"
-          :src="`/images/refactoring-ui/${props.double.sort[0]}.png`"
-          alt="label"
-        />
-
-        <img
-          class="w-8 h-8"
-          :src="`/images/refactoring-ui/${props.double.sort[0]}.png`"
-          alt="label"
-        />
-      </div>
-
-      <div v-if="single" class="flex">
-        <img
-          class="w-8 h-8"
-          :src="`/images/refactoring-ui/${props.single}.png`"
-          alt="label"
-        />
-      </div>
-
-      <img :src="`/images/refactoring-ui/${props.name}`" alt="main" />
-    </div>
-  </a>
+  <Image
+    class="object-contain !w-full !h-full"
+    :src="`/images/refactoring-ui/${props.name}`"
+    alt="main"
+  />
 </template>
 
 <script setup lang="ts">
+import { Image } from "ant-design-vue/es";
 interface Props {
   name: string;
-  single: "bad" | "good" | "question";
-  double: {
-    sort: ["bad", "good"] | ["good", "bad"];
-    gap: number;
-  };
 }
 
 const props = defineProps<Props>();
